17f8a6dfc161f95757c6769deb6d05f9c7a328fb
Jang Belche <jang.belche@student.manchester.ac.uk>
2021-02-13 21:41:00 +0000

    integrate skeleton

--- src/Main.js
+++ src/Main.js
@@ -1,33 +1,60 @@
 import React, { Component } from 'react';
 import firebase from './util/firebase';
-
+import './main.css';
 
 class Main extends Component{
 
     constructor(props) {
         super(props);
         this.state = {
-
+            snap: null,
+            loaded:false
         }
-        this.createTodo = this.createTodo.bind(this);
     }
 
-    createTodo(){
-        const todoRef = firebase.database().ref('water');
-       
+    componentDidMount(){
+        const todoRef = firebase.database().ref('water');       
 
         //todoRef.push(todo);
         todoRef.on('value', (snapshot) => {
             console.log(snapshot.val());
+            this.setState({
+                snap: snapshot.val(),
+                loaded: true
+            })
         })
     }
 
     render(){
-        return(
-            <div>
-                <button onClick={this.createTodo}>click</button>
+        if(!this.state.loaded){
+            return(
+            <div className="Homepage">
+                Loading
             </div>
-        )
+            )
+        }else{
+            const snap = this.state.snap
+            return(
+                <div className="Homepage">
+                    <h1>TreeHacks</h1>
+                        <table style={{width:'20%', marginLeft:'40%'}}>
+                            <tr>
+                                <th>activity</th>
+                                <th>amount</th>
+                            </tr>
+
+                            {Object.keys(snap).map((item, index) => (
+                                
+                                <tr>
+                                    <td key={index}>{item}</td>
+                                    <td key={item}>{snap[item]}</td>
+                                </tr>                                
+                                
+                            ))}
+                        </table>
+                </div>
+            )
+        }
     }
 }
 
